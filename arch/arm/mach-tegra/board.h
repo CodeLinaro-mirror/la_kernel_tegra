2befb2ad0272187d7c394a99317dfcf30837a47a
Colin Cross <ccross@android.com>
2010-12-01 15:45:30 -0800

    ARM: tegra: dvfs: Allow boot or run time disabling of dvfs rails
    Change-Id: Ie56cbf4ade1bbdb5835851f3c09668c1e0941a2c
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -33,6 +33,7 @@ void __init tegra_protected_aperture_init(unsigned long aperture);
 void __init tegra_move_framebuffer(unsigned long to, unsigned long from,
 	unsigned long size);
 int __init tegra_pcie_init(bool init_port0, bool init_port1);
+int tegra_dvfs_rail_disable_by_name(const char *reg_id);
 
 extern unsigned long tegra_bootloader_fb_start;
 extern unsigned long tegra_bootloader_fb_size;
